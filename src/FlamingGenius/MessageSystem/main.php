b58e92a0d8d12f6bfb187f1157b09068ef9129d1
FlamingGenius <livewiretech.reply.mail@gmail.com>
2015-10-01 02:29:49 -0400

    Update main.php

--- src/FlamingGenius/MessageSystem/main.php
+++ src/FlamingGenius/MessageSystem/main.php
@@ -7,6 +7,7 @@ use pocketmine\command\CommandSender;
 use pocketmine\utils\Config;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
+use pocketmine\utils\TextFormat;
 
 
 class main extends PluginBase{
@@ -24,49 +25,49 @@ class main extends PluginBase{
    $tag = $this->getConfig()->get("broadcast-tag");
    $color = $this->getConfig()->get("broadcast-color");
    $mcolor = $this->getConfig()->get("brm-color");
-   $this->getServer()->broadcastMessage($color . $tag . " " . $msg);
+   $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
   }
 
   elseif(strtolower($cmd) == "bgod"){
    $tag = $this->getConfig()->get("bgod-tag");
    $color = $this->getConfig()->get("bgod-color");
    $mcolor= $this->getConfig()->get("bgm-color");
-   $this->getServer()->broadcastMessage($color . $tag . " " . $msg);
+   $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
   }
 
   elseif(strtolower($cmd) == "bhero"){
    $tag = $this->getConfig()->get("bhero-tag");
    $color = $this->getConfig()->get("bhero-color");
    $mcolor= $this->getConfig()->get("bhm-color");
-   $this->getServer()->broadcastMessage($color . $tag . " " . $msg);
+   $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
   }
 
   elseif(strtolower($cmd) == "announce"){
    $tag = $this->getConfig()->get("announce-tag");
    $color = $this->getConfig()->get("announce-color");
    $mcolor= $this->getConfig()->get("anm-color");
-   $this->getServer()->broadcastMessage($color . $tag . " " . $msg);
+   $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
   }
 
   elseif(strtolower($cmd) == "server"){
    $tag = $this->getConfig()->get("server-tag");
    $color = $this->getConfig()->get("server-color");
    $mcolor= $this->getConfig()->get("sem-color");
-   $this->getServer()->broadcastMessage($color . $tag . " " . $msg);
+   $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
   }
 
   elseif(strtolower($cmd) == "console"){
    $tag = $this->getConfig()->get("console-tag");
    $color = $this->getConfig()->get("console-color");
    $mcolor= $this->getConfig()->get("com-color");
-   $this->getServer()->broadcastMessage($color . $tag . " " . $msg);
+   $this->getServer()->broadcastMessage($color . $tag . $mcolor ." " . $msg);
   }
   
   elseif(strtolower($cmd) == "bcia"){
    $tag = $this->getConfig()->get("bcia-tag");
    $color = $this->getConfig()->get("bcia-color");
    $mcolor= $this->getConfig()->get("bcm-color");
-   $this->getServer()->broadcastMessage($color . $tag . " " . $msg);
+   $this->getServer()->broadcastMessage($color . $tag . $mcolor . " " . $msg);
   }
 
 
